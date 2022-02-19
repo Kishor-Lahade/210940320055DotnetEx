9fb22c10f6b4c570f9f2b26e53d0782f3a227ae0
Kishor-Lahade <92206744+Kishor-Lahade@users.noreply.github.com>
2022-02-19 18:45:57 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _210940320055.Models
+{
+    public class Product
+    {
+        
+        [Key]
+        public int ProductId { get; set; }
+
+       
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Enter Product  name")]
+        public string ProductName { get; set; }
+
+        [DataType(DataType.Text)]
+        
+        public Decimal Rate { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Enter Description")]
+        public string Description { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Enter Category name")]
+        public string CategoryName{get;set;}
+
+    }
+}
